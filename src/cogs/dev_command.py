fd5955fd8d15fce2cc74a943489fc4ee10154dfa
AlexNg <taboez4life@gmail.com>
2023-12-14 16:09:37 +0800

    Update cog name

--- src/cogs/dev_command.py
+++ src/cogs/dev_command.py
@@ -29,20 +29,20 @@ class DevCommands(commands.Cog, command_attrs = dict(hidden = True), group_extra
   @commands.command()
   @Protected.legacy(PermissionPreset.Developer)
   async def load(self, ctx: commands.Context, extension):
-    await self.client.load_extension(f'cogs.{extension}')
+    await self.client.load_extension(f'.cogs.{extension}', package = 'src')
     await ctx.message.add_reaction('✅')
 
   @commands.command()
   @Protected.legacy(PermissionPreset.Developer)
   async def unload(self, ctx: commands.Context, extension):
-    await self.client.unload_extension(f'cogs.{extension}')
+    await self.client.unload_extension(f'.cogs.{extension}', package = 'src')
     await ctx.message.add_reaction('✅')
 
   @commands.command()
   @Protected.legacy(PermissionPreset.Developer)
   async def reload(self, ctx: commands.Context, extension):
-    await self.client.unload_extension(f'cogs.{extension}')
-    await self.client.load_extension(f'cogs.{extension}')
+    await self.client.unload_extension(f'.cogs.{extension}', package = 'src')
+    await self.client.load_extension(f'.cogs.{extension}', package = 'src')
     await ctx.message.add_reaction('✅')
   
 
